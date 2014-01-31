288ed9cc4b5c8a43b07ff8d2bfc9dea3542bd2af
root <root@raspberrypi.(none)>
2014-01-31 00:34:11 -0800

    runSteadyTests script runs tests for 0 disturbance and 0-1023 control with 50 step printed granularity to steadytests.txt

---
+++ steadytest.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+//int plant(int disturbance, int control);
+
+int main (int argc, char * argv[]) {    // used to characterize the dynamics of the unknown plant
+	int i, j, result;
+	int disturbance = 0;
+	int control = atoi(argv[1]);
+	FILE *fp;
+	fp = fopen("steadytests.txt", "a");
+	if(fp == NULL) exit (-1);
+	fprintf(fp, "Control: %d\n", control);
+	fprintf(fp, "Disturbance: %d\n\n", disturbance);
+	fprintf(fp, "Step	Output\n");
+	int plant(int disturbance, int control);
+	for(i=0;i<=1000;i++) {
+		// when you have the ADC and DAC interfaces completed you will get and put values like this:
+		// result = plant(getDisturb(), getControl()); // get these signals from respective ADCs
+		result = plant(disturbance, control);    // these can be varied from run to run to see how the plant behaves
+		// then
+		// putResult(result);    // to DAC output
+		// printf("Level: %d\n", result);
+		if(i%50==0){
+			fprintf(fp, "%d	%d\n", i, result);
+		}
+	}
+	fprintf(fp, "---------------------\n\n");
+	fclose(fp);
+	return 0;
+}
